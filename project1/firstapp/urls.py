9edbe65d11bb911fe66d741cc62285a112564764
Name <email@example.ru>
2022-01-09 19:18:54 +0100

    Initial commit

--- project1/firstapp/urls.py
+++ project1/firstapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ArticleList, ArticleDetail  # импортируем наше представление
+from .views import ArticleList, ArticleDetail, NewsList , NewsDetail  # импортируем наше представление
 
 urlpatterns = [
     # path — означает путь. В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно, почему
     path('', ArticleList.as_view()),
-    path('<int:pk>', ArticleDetail.as_view())
+    path('<int:pk>', ArticleDetail.as_view()),
+    path('', NewsList.as_view()),
+    path('<int:pk>', NewsDetail.as_view())
     # т. к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
 ]
--- project1/firstapp/views.py
+++ project1/firstapp/views.py
@@ -1,14 +1,14 @@
 from django.views.generic import \
     ListView, \
     DetailView  # импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
-from .models import Article
+from .models import Article, News
 import datetime
 
 
 class ArticleList(ListView):
     model = Article  # указываем модель, объекты которой мы будем выводить
-    template_name = 'index.html'  # указываем имя шаблона, в котором будет лежать HTML, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
-    context_object_name = 'article'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+    template_name = 'article.html'  # указываем имя шаблона, в котором будет лежать HTML, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    context_object_name = 'Article'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -20,5 +20,24 @@ class ArticleList(ListView):
 
 class ArticleDetail(DetailView):
     model = Article  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
-    template_name = 'index.html'  # название шаблона будет product.html
+    template_name = 'article.html'  # название шаблона будет product.html
     context_object_name = 'Article'  # название объекта. в нём будет
+
+
+class NewsList(ListView):
+    model = News  # указываем модель, объекты которой мы будем выводить
+    template_name = 'article.html'  # указываем имя шаблона, в котором будет лежать HTML, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    context_object_name = 'News'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.datetime.utcnow().strftime(
+            '%B %d %Y - %H:%M:%S')  # добавим переменную текущей даты time_now
+        context['value1'] = None
+        return context
+
+
+class NewsDetail(DetailView):
+    model = News  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'article.html'  # название шаблона будет product.html
+    context_object_name = 'News'  # название объекта. в нём будет
